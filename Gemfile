740de856c43e54aa6e1d4483517dea198b48f64a
Huy Nguyen <hcn9@cornell.edu>
2025-03-05 11:38:17 -0800

    Fix Rubocop offenses

--- Gemfile
+++ Gemfile
@@ -16,8 +16,8 @@ gem "turbo-rails"
 gem "stimulus-rails"
 # Build JSON APIs with ease [https://github.com/rails/jbuilder]
 gem "jbuilder"
-# Add csv for seeding data 
-gem 'csv'
+# Add csv for seeding data
+gem "csv"
 
 # Use Active Model has_secure_password [https://guides.rubyonrails.org/active_model_basics.html#securepassword]
 # gem "bcrypt", "~> 3.1.7"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 require 'csv'
 
-fortunes_text = File.read(Rails.root.join('lib','seeds','fortunes.csv'))
-fortunes_csv = CSV.parse(fortunes_text, liberal_parsing: true, :headers => true, quote_char: '"')
+fortunes_text = File.read(Rails.root.join('lib', 'seeds', 'fortunes.csv'))
+fortunes_csv = CSV.parse(fortunes_text, liberal_parsing: true, headers: true, quote_char: '"')
 fortunes_csv.each do |row|
     f = Fortune.new
     f.text = row['text']
